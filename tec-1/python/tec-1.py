0554338c31cb221ddd3eeb5ce7c4d13cb308c13e
Tom Keddie <github@bronwenandtom.com>
2021-10-12 18:15:12 -0700

    hardware/gals: first working ftdi driven keyboard

--- tec-1/python/tec-1.py
+++ tec-1/python/tec-1.py
@@ -1,6 +1,26 @@
-import sys,logging
+import time,sys,logging
 from pyftdi.gpio import Ftdi, GpioAsyncController
 from pyftdi.eeprom import FtdiEeprom
+
+c_plus  = 0b0001_0000
+c_minus = 0b0001_0001
+c_go    = 0b0001_0010
+c_ad    = 0b0001_0011
+
+def write_byte(gpio, data):
+    print("0x{:02x} -> 0x{:02x}".format(data, 0b0100_0000 | ((data >> 4) & 0b0000_1111)))
+    # nmi low & upper nibble
+    gpio.write(0b0100_0000 | ((data >> 4) & 0b0000_1111))
+    gpio.write(0b0000_0000 | ((data >> 4) & 0b0000_1111))
+    # nmi low & lower nibble
+    gpio.write(0b0100_0000 | ((data >> 0) & 0b0000_1111))
+    gpio.write(0b0000_0000 | ((data >> 0) & 0b0000_1111))
+    # nmi low & plus key
+    gpio.write(0b0100_0000 | c_plus)
+    gpio.write(0b0000_0000 | c_plus)
+    
+
+
 
 if len(sys.argv) < 4:
     print("{} <ftdi_type> <ftdi_serialnumber> <ftdi_instance>\n".format(sys.argv[0]))
@@ -12,26 +32,23 @@ ftdi_instance = sys.argv[3]
 
 url = "ftdi://ftdi:{}:{}/{}".format(ftdi_type, ftdi_serial, ftdi_instance)
 
-gpio = GpioAsyncController()
-gpio.configure(url, direction=0b1111_1111)
-for ix in range(0, 100):
-    gpio.write(ix)
 
-sys.exit(0)
-ftdi = Ftdi()
-#ftdi.open_from_url(url)
-ftdi.open_bitbang_from_url(url, 0b1111_1111)
 
+# ftdi = Ftdi()
+# ftdi.open_from_url(url)
 # eeprom = FtdiEeprom()
 # eeprom.connect(ftdi)
 # print("cbus mask {}".format(eeprom.cbus_mask))
+# ftdi.close()
+ 
+gpio = GpioAsyncController()
+gpio.configure(url, direction=0b1111_1111)
 
-# ftdi.set_bitmode(0b1111_1111, Ftdi.BitMode.BITBANG)# Ftdi.BitMode.CBUS)
-# ftdi.set_cbus_direction(0b1111, 0b1111)
+write_byte(gpio, 0x23)
 
-ftdi.write_data(bytes(0b1100_0000))
-#ftdi.set_cbus_gpio(0b0000)
-#ftdi.set_cbus_gpio(0b0001)
-#ftdi.set_cbus_gpio(0b0000)
+sys.exit(0)
 
+ftdi = Ftdi()
+ftdi.open_from_url(url)
+ftdi.open_bitbang_from_url(url, 0b1111_1111)
 ftdi.close()
