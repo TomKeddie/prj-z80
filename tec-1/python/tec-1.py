63e48fea4229394e9edecc8a632e737e83ffbf3b
Tom Keddie <github@bronwenandtom.com>
2021-10-14 19:10:31 -0700

    hardware/gals/keybd: cleanup nmi generation

--- tec-1/python/tec-1.py
+++ tec-1/python/tec-1.py
@@ -1,5 +1,5 @@
 import time,sys,logging
-from pyftdi.gpio import Ftdi, GpioAsyncController
+from pyftdi.gpio import Ftdi, GpioAsyncController, GpioSyncController
 from pyftdi.eeprom import FtdiEeprom
 
 c_plus  = 0b0001_0000
@@ -8,18 +8,21 @@ c_go    = 0b0001_0010
 c_ad    = 0b0001_0011
 
 def write_byte(gpio, data):
-    print("0x{:02x} -> 0x{:02x}".format(data, 0b0100_0000 | ((data >> 4) & 0b0000_1111)))
     # nmi low & upper nibble
-    gpio.write(0b0100_0000 | ((data >> 4) & 0b0000_1111))
-    gpio.write(0b0000_0000 | ((data >> 4) & 0b0000_1111))
+    gpio.exchange(0b1100_0000 | ((data >> 4) & 0b0000_1111))
+    gpio.exchange(0b0100_0000 | ((data >> 4) & 0b0000_1111))
     # nmi low & lower nibble
-    gpio.write(0b0100_0000 | ((data >> 0) & 0b0000_1111))
-    gpio.write(0b0000_0000 | ((data >> 0) & 0b0000_1111))
+    gpio.exchange(0b1100_0000 | ((data >> 0) & 0b0000_1111))
+    gpio.exchange(0b0100_0000 | ((data >> 0) & 0b0000_1111))
     # nmi low & plus key
-    gpio.write(0b0100_0000 | c_plus)
-    gpio.write(0b0000_0000 | c_plus)
+    gpio.exchange(0b1100_0000 | c_plus)
+    gpio.exchange(0b0100_0000 | c_plus)
     
-
+def enable(gpio):
+    gpio.exchange(0b0100_0000)
+    
+def disable(gpio):
+    gpio.exchange(0b0000_0000)
 
 
 if len(sys.argv) < 4:
@@ -32,23 +35,14 @@ ftdi_instance = sys.argv[3]
 
 url = "ftdi://ftdi:{}:{}/{}".format(ftdi_type, ftdi_serial, ftdi_instance)
 
-
-
-# ftdi = Ftdi()
-# ftdi.open_from_url(url)
-# eeprom = FtdiEeprom()
-# eeprom.connect(ftdi)
-# print("cbus mask {}".format(eeprom.cbus_mask))
-# ftdi.close()
- 
-gpio = GpioAsyncController()
+gpio = GpioSyncController()
 gpio.configure(url, direction=0b1111_1111)
+gpio.set_frequency(2_000_000)
+
+enable(gpio)
 
 write_byte(gpio, 0x23)
 
-sys.exit(0)
+disable(gpio)
 
-ftdi = Ftdi()
-ftdi.open_from_url(url)
-ftdi.open_bitbang_from_url(url, 0b1111_1111)
-ftdi.close()
+gpio.close()
